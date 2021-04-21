a9624cec18bb7dc41b830d9b597a02b47d4a1974
youngjae <shiphot300@naver.com>
2021-04-21 12:14:54 +0900

    make MyRouter.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const http = require('http');
+const myServer = require('./Myserver');
+const myServer = require('./MyRouter');
 
-function onRequest(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello, world');
-    res.end();
-}
-
-server = http.createServer(onRequest);
-server.listen(8000, 'localhost');
-console.log('Server is running at http://localhost:8000');
+myServer.start(myRouter.route);
