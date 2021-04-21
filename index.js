86312e111563449c9592422aa182c69a093be03e
youngjae <shiphot300@naver.com>
2021-04-21 12:34:07 +0900

    make MyHandler.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const myServer = require('./Myserver');
-const myServer = require('./MyRouter');
+const myRouter = require('./MyRouter');
+const myHandler = require('./MyHandler');
 
-myServer.start(myRouter.route);
+let handle = {};
+handle['/'] = myHandler.start;
+handle['/hello'] = myHandler.hello;
+
+myServer.start(myRouter.route, handle);
