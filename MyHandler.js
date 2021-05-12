dcb7a9e8bf8e3e5876f22fe47cd883e089a6e426
youngjae <shiphot300@naver.com>
2021-05-12 11:20:25 +0900

    add nicknmae  page handing form post

--- MyHandler.js
+++ MyHandler.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const os = require('os');
+const queryString = require('querystring');
 
 function start(res) {
     let Body = '<head><meta charset = "UTF-8"/></head>'
@@ -9,8 +10,8 @@ function start(res) {
     Body += '<div><a href="/firstHtml">HTML 읽는 페이지</a></div>'
     Body += '<div><a href="/page">handler 없이 "/page"로 매핑하는 페이지</a></div>'
     Body += '<div><a href="/serverInfo">Server 정보를 표시하는 페이지</a></div>'
-    Body += '<div><a href="/randomwait"></a></div>'
-    Body += '<div><a href="/randomwait"></a></div>'
+    Body += '<div><a href="/form">form을 입력하는 페이지</a></div>'
+    Body += '<div><a href="/nickname">form으로 넘어온 이름과 별명 표시 페이지</a></div>'
     Body += '<div><a href="/randomwait"></a></div>'
     Body += '<div><a href="/randomwait"></a></div>'
 
@@ -61,6 +62,16 @@ function serverInfo(res) {
     info = JSON.stringify(os.cpus());
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(info);
+    res.end();
+}
+
+function nickname(res, postData) {
+    let Body = '<head><meta charset = "UTF-8"/></head>';
+    Body += '<div>안녕하세요, ' + queryString.parse(postData).myName + '님.</div>'
+    Body += '<div>당신의별명은, ' + queryString.parse(postData).myNick + '입니다..</div>'
+    Body += '</html>'
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(Body);
     res.end();
 }
 
--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "add form input page "
+git commit -m "add nicknmae  page handing form post "
 git push -u origin master
